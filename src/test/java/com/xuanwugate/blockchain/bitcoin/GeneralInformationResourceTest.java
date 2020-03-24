9e20fe99520262ac0f8bf369bcaa01ca10eb15c1
Deshi Xiao <xiaods@gmail.com>
2020-03-24 23:00:59 +0800

    tuning btc info test

--- src/test/java/com/xuanwugate/blockchain/bitcoin/GeneralInformationResourceTest.java
+++ src/test/java/com/xuanwugate/blockchain/bitcoin/GeneralInformationResourceTest.java
@@ -1,75 +1,50 @@
 package com.xuanwugate.blockchain.bitcoin;
 
 import io.quarkus.test.junit.QuarkusTest;
-import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import com.xuanwugate.authentication.jwt.GenerateTokenResourceTest;
-import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class GeneralInformationResourceTest {
     @Test
     public void testGetNodeInformationEndpoint() {
-        GenerateTokenResourceTest token = new GenerateTokenResourceTest();
-        final String token_id = token.getJWTGenerateToken();
-        final Response res = given()
-        .header("X-API-KEY", "your-api-key")
-        .header("Authorization", "Bearer "+token_id)
-        .get("http://localhost:8080/v1/bc/btc/mainnet/info").prettyPeek();
-        final JsonPath bodyJson = res.getBody().jsonPath();
-        final int errorCode = bodyJson.getInt("errorCode");
-        final String payload = bodyJson.getString("payload");
-        res.then().statusCode(equalTo(200));
-        Assertions.assertTrue(errorCode == 0 && payload != null && !token_id.isEmpty());
+        given()
+          .header("X-API-KEY", "your-api-key")
+          .when().get("/v1/bc/btc/mainnet/info")
+          .then()
+             .statusCode(200)
+             .body(is("hello"));
     }
 
     @Test
     public void testGetBlockByHashEndpoint() {
-        GenerateTokenResourceTest token = new GenerateTokenResourceTest();
-        final String token_id = token.getJWTGenerateToken();
-        final Response res = given()
-        .header("X-API-KEY", "your-api-key")
-        .header("Authorization", "Bearer "+token_id)
-        .get("http://localhost:8080/v1/bc/btc/mainnet/blocks/00000000c937983704a73af28acdec37b049d214adbda81d7e2a3dd146f6ed09").prettyPeek();
-        final JsonPath bodyJson = res.getBody().jsonPath();
-        final int errorCode = bodyJson.getInt("errorCode");
-        final String payload = bodyJson.getString("payload");
-        res.then().statusCode(equalTo(200));
-        Assertions.assertTrue(errorCode == 0 && payload != null && !token_id.isEmpty());
+        given()
+          .header("X-API-KEY", "your-api-key")
+          .when().get("/v1/bc/btc/mainnet/blocks/00000000c937983704a73af28acdec37b049d214adbda81d7e2a3dd146f6ed09")
+          .then()
+          .statusCode(200)
+          .body(is("hello"));
     }
 
     @Test
     public void testGetBlockByHeightEndpoint() {
-        GenerateTokenResourceTest token = new GenerateTokenResourceTest();
-        final String token_id = token.getJWTGenerateToken();
-        final Response res = given()
-        .header("X-API-KEY", "your-api-key")
-        .header("Authorization", "Bearer "+token_id)
-        .get("http://localhost:8080/v1/bc/btc/mainnet/blocks/564349").prettyPeek();
-        final JsonPath bodyJson = res.getBody().jsonPath();
-        final int errorCode = bodyJson.getInt("errorCode");
-        final String payload = bodyJson.getString("payload");
-        res.then().statusCode(equalTo(200));
-        Assertions.assertTrue(errorCode == 0 && payload != null && !token_id.isEmpty());
+        given()
+          .header("X-API-KEY", "your-api-key")
+          .when().get("/v1/bc/btc/mainnet/blocks/564349")
+          .then()
+          .statusCode(200)
+          .body(is("hello"));
     }
 
     @Test
     public void testGetBlockLasterEndpoint() {
-        GenerateTokenResourceTest token = new GenerateTokenResourceTest();
-        final String token_id = token.getJWTGenerateToken();
-        final Response res = given()
-        .header("X-API-KEY", "your-api-key")
-        .header("Authorization", "Bearer "+token_id)
-        .get("http://localhost:8080/v1/bc/btc/mainnet/blocks/latest").prettyPeek();
-        final JsonPath bodyJson = res.getBody().jsonPath();
-        final int errorCode = bodyJson.getInt("errorCode");
-        final String payload = bodyJson.getString("payload");
-        res.then().statusCode(equalTo(200));
-        Assertions.assertTrue(errorCode == 0 && payload != null && !token_id.isEmpty());
+        given()
+          .header("X-API-KEY", "your-api-key")
+          .when().get("/v1/bc/btc/mainnet/blocks/latest")
+          .then()
+          .statusCode(200)
+          .body(is("hello"));
     }
 }
